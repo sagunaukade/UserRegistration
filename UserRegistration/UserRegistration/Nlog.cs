b4340cf2150c184a4b284755799c9f8681eeb30d
sagunaukade <sagunaukade96@gmail.com>
2022-02-17 23:14:19 +0530

    [Saguna] Add log file using Nlog nugget packages

---
+++ UserRegistration/UserRegistration/Nlog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NLog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Nlog
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
+
+        //Method to write success info to a Logfile
+        public static void SuccessInfo(string message)
+        {
+            logger.Info(message);
+        }
+        //Method to write error info to a Logfile
+        public static void ErrorInfo(string message)
+        {
+            logger.Error(message);
+        }
+    }
+}
+
+    
+
