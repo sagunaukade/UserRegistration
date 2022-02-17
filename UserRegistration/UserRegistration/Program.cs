ca2e545ee1272cb3a572356db68e0fe316c28551
sagunaukade <sagunaukade96@gmail.com>
2022-02-17 17:37:32 +0530

    [Saguna] Add regex pattern to validate FirstName

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   //Display Welcome message
+            Console.WriteLine("Welcome to the user registration programs");
+            //UC-1 
+            UserInfo.ValidateFirstName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration
+{
+    public class UserInfo
+    {
+        //UC-1 valid first name
+        public static void ValidateFirstName()
+        {   
+            Console.WriteLine("\nPlease Enter your First Name:");
+            string firstName = Console.ReadLine();
+            //First name starts with Cap and has minimun 3 characters
+            string firstName_pattern = "^[A-Z][a-zA-Z]{2,}$";
+
+            if (Regex.IsMatch(firstName, firstName_pattern))
+            {
+                Console.WriteLine("First Name is Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is InValid");
+                ValidateFirstName();
+            }
+        }
+    }
+}
