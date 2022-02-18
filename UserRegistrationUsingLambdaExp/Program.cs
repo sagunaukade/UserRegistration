560125dd482332c8eeadde9cacd3732158eb6f2e
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-18 23:57:13 +0530

    [Saguna] Using Lambda Expression

---
+++ UserRegistrationUsingLambdaExp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambdaExp
+{
+    internal class Program
+    {
+        //Instance Variable
+        public static string firstName, result;
+        public static string lastName;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the User Registration using Lambda Expression");
+            Console.WriteLine("Choose an Option \n 1. First Name ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    //first letter should be in capital format
+                    Console.WriteLine("Enter your first name");
+                    firstName = Console.ReadLine();
+                    RegexPattern.CheckFirstName(firstName);
+                    break;
+                case 2:
+                    //first letter should be in capital format
+                    Console.WriteLine("Enter your Last Name");
+                    lastName = Console.ReadLine();
+                    RegexPattern.CheckLastName(lastName);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationUsingLambdaExp/RegexPattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambdaExp
+{
+    internal class RegexPattern
+    {
+        public static Func<string, string> CheckFirstName = (firstName) =>
+        {
+            // first character in capital and minimum 3 characters name
+            string pattern = "(^[^a-z][A-z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(firstName, pattern))
+            {
+                Console.WriteLine("Name is valid");
+                return firstName;
+            }
+            else
+            {
+                Console.WriteLine("Name is Invalid ");
+            }
+            return firstName;
+        };
+        public static Func<string, string> CheckLastName = (lastName) =>
+        {
+            //first character in capital and minimum 3 characters name
+            string pattern = "(^[^a-z][A-z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(lastName, pattern))
+            {
+                Console.WriteLine("Name is valid");
+                return lastName;
+            }
+            else
+            {
+                Console.WriteLine("name is Invalid");
+            }
+            return lastName;
+        };
+    }
+}
+
