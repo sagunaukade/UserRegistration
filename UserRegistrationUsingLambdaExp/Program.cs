4807da6a5f537b7cdf6f901617de13597e2a6743
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-20 00:53:04 +0530

    [Saguna] Valid mobile format using lambda expression

---
+++ UserRegistrationUsingLambdaExp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambdaExp
+{
+    internal class Program
+    {
+        //Instance Variable
+        public static string firstName, lastName, email;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the User Registration using Lambda Expression");
+            Console.WriteLine("Choose an Option:\n1. First Name \n2.Last Name \n3.Email \n4.Mobile Number");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    //first letter should be in capital format
+                    Console.WriteLine("Enter your first name");
+                    firstName = Console.ReadLine();
+                    RegexPattern.CheckFirstName(firstName);
+                    break;
+                case 2:
+                    //first letter should be in capital format
+                    Console.WriteLine("Enter your Last Name");
+                    lastName = Console.ReadLine();
+                    RegexPattern.CheckLastName(lastName);
+                    break;
+                case 3:
+                    //UC-3
+                    Console.WriteLine("Enter your email");
+                    email = Console.ReadLine();
+                    RegexPattern.CheckEmail(email);
+                    break;
+                case 4:
+                    //UC-4
+                    Console.WriteLine("Enter your mobile number");
+                    string mobileNumber = Console.ReadLine();
+                    RegexPattern.CheckMobileNumber(mobileNumber);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationUsingLambdaExp/RegexPattern.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambdaExp
+{
+    internal class RegexPattern
+    {
+        public static Func<string, string> CheckFirstName = (firstName) =>
+        {
+            // first character in capital and minimum 3 characters name
+            string pattern = "(^[^a-z][A-z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(firstName, pattern))
+            {
+                Console.WriteLine("Name is valid");
+                return firstName;
+            }
+            else
+            {
+                Console.WriteLine("Name is Invalid ");
+            }
+            return firstName;
+        };
+        public static Func<string, string> CheckLastName = (lastName) =>
+        {
+            //first character in capital and minimum 3 characters name
+            string pattern = "(^[^a-z][A-z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(lastName, pattern))
+            {
+                Console.WriteLine("Name is valid");
+                return lastName;
+            }
+            else
+            {
+                Console.WriteLine("name is Invalid");
+            }
+            return lastName;
+        };
+        //UC-3
+        public static Func<string, string> CheckEmail = (email) =>
+        {
+            string pattern = "^[a-zA-Z0-9]{3,}([._+-][0-9a-zA-Z]{2,})*@[0-9a-zA-Z]+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$";
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine("entered email is valid");
+                Nlog.SuccessLog("Entered mail is valid  " + email);
+                return email;
+            }
+            else
+            {
+                Console.WriteLine("Entered email is Invalid");
+                Nlog.ErrorInfo("Entered mail not valid  " + email);
+            }
+            return email;
+        };
+        //UC-4
+        public static Func<string, string> CheckMobileNumber = (mobileNumber) =>
+        {
+            string pattern = "^91[ ][1-9][0-9]{9}$";
+            if (Regex.IsMatch(mobileNumber, pattern))
+            {
+                Console.WriteLine("Mobile number is valid");
+                Nlog.SuccessLog("Entered mobile number is valid  " + mobileNumber);
+                return mobileNumber;
+            }
+            else
+            {
+                Console.WriteLine("Entered mobile number is invalid");
+                Nlog.ErrorInfo("Entered mobile number is invalid  " + mobileNumber);
+                return mobileNumber;
+            }
+        };
+    }
+}
+
